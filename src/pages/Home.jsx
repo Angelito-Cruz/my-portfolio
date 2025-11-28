01646fa2bb4c5cc177c2c38d0f0036cdbfecc4d4
Enzo <enzourbano122@gmail.com>
2025-11-28 13:09:19 +0800

    Commite all changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,9 @@ const projects = [
     link: 'https://appetize.io/embed/b_clf3yzcnyyg5af25nlkyh7d7ty?device=pixel9pro&launchUrl=exp%3A%2F%2Fu.expo.dev%2F933fd9c0-1666-11e7-afca-d980795c5824%3Fruntime-version%3Dexposdk%253A54.0.0%26channel-name%3Dproduction%26snack%3D%2540araiku%252Fportfolio%26snack-channel%3DMfbPOtFRbI&params=%7B%22EXDevMenuDisableAutoLaunch%22%3Atrue%2C%22EXKernelDisableNuxDefaultsKey%22%3Atrue%7D&appearance=dark&deviceColor=white&scale=auto&orientation=portrait&centered=both'
   },
   {
-    title: 'SmartSched',
-    description: 'AI-powered scheduling system.',
-    link: 'https://your-smartsched.vercel.app'
+    title: 'Car Car',
+    description: 'Car Maintenance Tracker.',
+    link: 'https://carmaintenancetracker.vercel.app/'
   }
 ];
 
